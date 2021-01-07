b274cdec83ec8de4ea29028c260e2af292f70947
vh181 <dinizvictorhugo18@gmail.com>
2021-01-06 21:44:32 -0300

    d

--- app/src/main/java/com/gv/recyclermasterkotlin/EmailAdapter.kt
+++ app/src/main/java/com/gv/recyclermasterkotlin/EmailAdapter.kt
@@ -1,5 +1,6 @@
 package com.gv.recyclermasterkotlin
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,17 +8,21 @@ import com.gv.recyclermasterkotlin.model.Email
 
 class EmailAdapter(val emails: MutableList<Email>): RecyclerView.Adapter<EmailAdapter.EmailViewHolder>(){
     inner class EmailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(email: Email) {
+
+        }
 
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmailViewHolder {
-        TODO("Not yet implemented")
+     val view = LayoutInflater.from(parent.context).inflate(R.layout.email_item,parent,false)
+        return EmailViewHolder(view)
     }
 
 
     override fun onBindViewHolder(holder: EmailViewHolder, position: Int) {
-        TODO("Not yet implemented")
+  holder.bind(emails[position])
     }
 
 
